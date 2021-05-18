5dbcf08cd14a0f334e91670c46aca591b01679c2
houh <Hedy.Hou@anz.com>
2021-05-18 16:18:20 +0800

    get recommend data

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>this is recommend page</div>
+    <div class='recommend'>this is recommend page</div>
 </template>
+<script>
+import { getRecommend } from '@/service/recommend'
+export default {
+    name: 'recommend',
+    async created () {
+        const result = await getRecommend()
+        console.log(result)
+    }
+}
+</script>
